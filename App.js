bccd464bbcea36d338d58e12dc792725e40887f9
Barry Mullan <mullan@gmail.com>
2016-01-15 07:16:34 -0500

    updates

--- App.js
+++ App.js
@@ -4,12 +4,11 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     itemId : 'app',
-
+    cache : [],
     items : [{
         itemId : 'panel',
     	xtype : 'panel',
     	layout : 'column',
-
     	items : [
     	{
             name: 'intervalType',
@@ -75,7 +74,24 @@ Ext.define('CustomApp', {
 	        xtype: 'datefield',
 	        fieldLabel: 'End Date',
 	        value: new Date()
-    	}
+    	},
+        {
+            itemId : 'exportButton',
+            margin: '5 5 5 5',
+            xtype: 'rallybutton',
+            text : 'Export',
+            handler : function() {
+                // export
+                var grid = app.down("#grid");
+                // console.log("grid",grid);
+                var link = app.down("#exportLink");
+                // console.log("link",link);
+                link.update(app.exporter.exportGrid(grid));
+            }
+        },
+        {
+            itemId : "exportLink", margin : "12 0 0 10"
+        }
     	]
     }],
 
@@ -89,8 +105,6 @@ Ext.define('CustomApp', {
     timeEntryItemColumn : {  
         text: "Time Entry Item", width:100, 
         renderer : function(value, metaData, record, rowIdx, colIdx, store, view) {
-            if (rowIdx===0)
-                console.log(record);
             var item = record.get("TimeEntryItemObject");
             return item ? item.get("WorkProductDisplayString") : "";
        }
@@ -107,24 +121,22 @@ Ext.define('CustomApp', {
     epicColumn : {  
         text: "Epic",
         renderer : function(value, metaData, record, rowIdx, colIdx, store, view) {
-            var item = record.get("TimeEntryItemObject");
-            return item ? item.get("ProjectDisplayString") : "";
+            var item = record.get("EpicObject");
+            return item ? item.get("FormattedID") + ":" + item.get("Name") : "";
        }
     },
 
     featureColumn : {  
         text: "Feature",
         renderer : function(value, metaData, record, rowIdx, colIdx, store, view) {
-            var item = record.get("TimeEntryItemObject");
-            return item ? item.get("ProjectDisplayString") : "";
+            var item = record.get("FeatureObject");
+            return item ? item.get("FormattedID") + ":" + item.get("Name") : "";
        }
     },
 
     taskColumn : {  
         text: "Task", width:100, 
         renderer : function(value, metaData, record, rowIdx, colIdx, store, view) {
-            if (rowIdx===0)
-                console.log(record);
             var item = record.get("TimeEntryItemObject");
             return item ? item.get("TaskDisplayString") : "";
        }
@@ -133,8 +145,6 @@ Ext.define('CustomApp', {
     userColumn : {  
         text: "User", width:100, 
         renderer : function(value, metaData, record, rowIdx, colIdx, store, view) {
-            if (rowIdx===0)
-                console.log(record);
             var item = record.get("TimeEntryItemObject");
             return item ? item.get("User")._refObjectName : "";
        }
@@ -142,6 +152,8 @@ Ext.define('CustomApp', {
 
 
 	_onLoad: function() {
+
+        app.exporter = Ext.create("GridExporter");
 
         var cols = [
             { dataIndex : 'DateVal', text: 'Date'},
@@ -158,6 +170,7 @@ Ext.define('CustomApp', {
 
 	    this.add({
 	        xtype: 'rallygrid',
+            itemId : 'grid',
 	        columnCfgs: cols,
 	        context: this.getContext(),
 	        storeConfig: {
@@ -166,11 +179,31 @@ Ext.define('CustomApp', {
             listeners:  {
                 load : function(rows) {
                     var values = rows.data.items;
-                    app.loadTimeEntryItems(values).then( {
+                    // time entry items
+                    app.readTimeEntryItems(values).then( {
                         success: function(items) {
                             console.log("items",items);
-                            _.each(values,function(v,i){
-                                v.set("TimeEntryItemObject",items[i]);
+                            app.setValues(values,items,"TimeEntryItemObject")
+                            // stories
+                            app.loadStories(items).then({
+                                success: function(stories) {
+                                    console.log("stories",stories);
+                                    app.setValues(values,stories,"StoryObject");
+                                    // features
+                                    app.readFeatures(stories).then({
+                                        success: function(features) {
+                                            console.log("features",features);
+                                            app.setValues(values,features,"FeatureObject");
+                                            // epics
+                                            app.readEpics(features).then({
+                                                success: function(epics) {
+                                                    console.log("epics",epics);
+                                                    app.setValues(values,epics,"EpicObject");
+                                                }
+                                            })
+                                        }
+                                    })
+                                }
                             });
                         }
                     });
@@ -179,30 +212,115 @@ Ext.define('CustomApp', {
 	    });
     },
 
-    loadTimeEntryItems : function(values) {
+    setValues : function(items, values, attrName) {
+        _.each(items,function(item,x){
+            item.set(attrName,values[x]);
+        });
+    },
 
-        var promises = _.map(values,function(v) {
-            var deferred = Ext.create('Deft.Deferred');
-            var objectId = v.get("TimeEntryItem");
-            console.log(v,"objectId",objectId);
-            
+    // TimesheetEntryItems -> Stories -> Features -> Epics
+
+    readObject : function(model,ref) {
+
+        var deferred = Ext.create('Deft.Deferred');
+        var obj = _.find( app.cache, function (cacheObj) {
+            if (cacheObj.ref._ref === ref._ref) {
+                return cacheObj.promise.promise;
+            }
+        });
+
+        if (!_.isUndefined(obj)&&!_.isNull(obj)) {
+            return obj.promise.promise;
+        } else {
             Rally.data.ModelFactory.getModel({
-                type: 'TimeEntryItem',
+                type : model,
                 success: function(model) {
-                    console.log("objectId",objectId);
-                    model.load(objectId,{
+                    model.load(ref,{
                         fetch : true,
                         callback : function(result,operation) {
                             deferred.resolve(result);
                         }
                     })
                 }
+            })
+            app.cache.push({ref : ref, promise: deferred});
+            return deferred.promise;
+        }
+    },
+
+    // passed an array of TimeEntryItems. If the entry is 
+    loadStories : function(items) {
+
+        var promises = _.map(items,function(item){
+            var deferred = Ext.create('Deft.Deferred');
+            var workProductRef = item.get("WorkProduct");
+            if (workProductRef===null || workProductRef._type !== "HierarchicalRequirement") {
+                deferred.resolve(null);
+            }
+            else {
+                app.readObject('HierarchicalRequirement',workProductRef).then({
+                    success : function(obj) {
+                        deferred.resolve(obj);
+                    }    
+                });
+            }
+            return deferred.promise;
+        });
+        return Deft.Promise.all(promises);
+    },
+
+    readTimeEntryItems : function(values) {
+        var promises = _.map(values,function(v) {
+            var deferred = Ext.create('Deft.Deferred');
+            var ref = v.get("TimeEntryItem");
+
+            app.readObject('TimeEntryItem',ref).then({
+                success : function(obj) {
+                    deferred.resolve(obj);
+                }    
             });
             return deferred.promise;
         });
         return Deft.Promise.all(promises);
-
     },
+
+
+    readFeatures : function(stories) {
+        var promises = _.map(stories,function(story){
+            var deferred = Ext.create('Deft.Deferred');
+            if (_.isNull(story) || _.isUndefined(story.get("Feature")) || _.isNull(story.get("Feature"))) {
+                deferred.resolve(null);
+            } else {
+                var featureRef = story.get("Feature");
+                app.readObject('PortfolioItem/Feature',featureRef).then({
+                    success : function(obj) {
+                        deferred.resolve(obj);
+                    }    
+                });
+            }
+            return deferred.promise;
+        });
+        return Deft.Promise.all(promises);
+    },
+
+    readEpics : function(features) {
+        var promises = _.map(features,function(feature){
+            var deferred = Ext.create('Deft.Deferred');
+            if (_.isNull(feature) || _.isUndefined(feature.get("Parent")) || _.isNull(feature.get("Parent")) ) {
+                deferred.resolve(null);
+            } else {
+                var epicRef = feature.get("Parent");
+                app.readObject('PortfolioItem/Initiative',epicRef).then({
+                    success : function(obj) {
+                        deferred.resolve(obj);
+                    }    
+                });
+            }
+            return deferred.promise;
+        });
+        return Deft.Promise.all(promises);        
+    },
+
 
     _getDateFilter: function() {
         var panel = _.first(Ext.ComponentQuery.query("#panel"));
@@ -232,6 +350,31 @@ Ext.define('CustomApp', {
 
     _onSelectDate : function(a,b,c) {
         console.log(a,b,c);
+    },
 
-    }
+    _export: function(){
+        var grid = this.down('rallygrid');
+        var me = this;
+
+        if ( !grid ) { return; }
+        
+        this.logger.log('_export',grid);
+
+        var filename = Ext.String.format('user-permissions.csv');
+
+        this.setLoading("Generating CSV");
+        Deft.Chain.sequence([
+            function() { return Rally.technicalservices.FileUtilities._getCSVFromCustomBackedGrid(grid) } 
+        ]).then({
+            scope: this,
+            success: function(csv){
+                if (csv && csv.length > 0){
+                    Rally.technicalservices.FileUtilities.saveCSVToFile(csv,filename);
+                } else {
+                    Rally.ui.notify.Notifier.showWarning({message: 'No data to export'});
+                }
+                
+            }
+        }).always(function() { me.setLoading(false); });
+    },
 });
